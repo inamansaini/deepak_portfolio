935b9a2f57a1635b91ea9d9894a3188136c1b267
Naman <namansaini720@gmail.com>
2025-10-28 21:11:08 +0530

    Your message describing all the updates

--- portfolio/portfolio.js
+++
@@ -1,8 +0,0 @@
-let section=document.querySelectorAll('section');
-let navlinks=document.querySelectorAll('header nav a');
-window.onscroll=()=>{
-    section.forEach(sec =>{
-        let top=window.scrollY;
-        let offset=sec.offsetTop
-    })
-}
